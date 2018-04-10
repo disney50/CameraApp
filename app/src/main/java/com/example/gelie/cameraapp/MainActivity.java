896637d63dfaecca9fb6b7599a662ca6525a1447
disney50 <36032319+disney50@users.noreply.github.com>
2018-04-10 11:26:29 +0200

    first commit

--- app/src/main/java/com/example/gelie/cameraapp/MainActivity.java
+++ app/src/main/java/com/example/gelie/cameraapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.gelie.cameraapp;
 
 
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.hardware.Camera;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -11,12 +14,21 @@ import android.widget.FrameLayout;
 
 import com.example.gelie.cameraapp.HardwareServices.CameraService;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static com.example.gelie.cameraapp.HardwareServices.CameraService.getCameraInstance;
 
 public class MainActivity extends AppCompatActivity {
 
     static Camera mCamera = null;
     private CameraView mCamView;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final String TAG = "CameraApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -52,13 +64,63 @@ public class MainActivity extends AppCompatActivity {
         );
     }
 
+    private static Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    private static File getOutputMediaFile(int type){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "CameraApp");
+
+        if (! mediaStorageDir.exists()) {
+            if (! mediaStorageDir.mkdirs()) {
+                Log.d(TAG, "failed to create directory");
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG" + timeStamp + ".jpg");
+        }
+        else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
     Camera.PictureCallback mPicture = new Camera.PictureCallback() {
         @Override
         public void onPictureTaken(byte[] data, Camera camera) {
 
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null) {
+                Log.d(TAG, "Error creating media file, check storage permissions" /*+ e.getMessage()*/);
+                return;
+            }
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
         }
     };
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();
+    }
 
-
+    private void releaseCamera() {
+        if (mCamera == null) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
 }
